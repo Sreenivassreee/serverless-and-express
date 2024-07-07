b27bad5f4140fac524407f8d5acd1c16a322284d
Sree <sree@Srees-MacBook-Air.local>
2024-07-07 12:11:42 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+app.post('/',(c)=>{
+return c.text('post')
+})
+
+export default app
